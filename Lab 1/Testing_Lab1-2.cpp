f304412961185951a00274045a2d1cb776f441f7
kylienic <kyliegnic@gmail.com>
2015-09-08 12:34:08 -0400

    reset counter variables properly

--- Lab 1/Testing_Lab1-2.cpp
+++ Lab 1/Testing_Lab1-2.cpp
@@ -65,15 +65,16 @@ int main(){
 				else{
 					countLetters(wordBuilder, numVowels, numCons, numOther);
 					wordBuilder.clear(); // reset word builder
-					numVowels = 0;
-					numCons=0;
-					numOther=0;
 				}
 			}
 			// upon for loop exit, entire sentence has been processed, so now output counts
 			cout<<numVowels<<" vowels"<<endl;
 			cout<<numCons<<" consonants"<<endl;
 			cout<<numOther<<" other"<<endl;
+			//reset counter variables
+			numVowels = 0;
+			numCons=0;
+			numOther=0;
 		}
 	}
 	return 0;
